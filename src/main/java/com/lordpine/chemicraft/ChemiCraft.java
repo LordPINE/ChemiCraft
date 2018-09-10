4eba8da54a8d1d77267ad23427cff1b649195718
LordPINE <middelhylke@gmail.com>
2018-09-10 19:50:04 +0200

    I'm a moron and didn't add the updated main mod file.

--- src/main/java/com/lordpine/chemicraft/ChemiCraft.java
+++ src/main/java/com/lordpine/chemicraft/ChemiCraft.java
@@ -5,23 +5,19 @@ import com.lordpine.chemicraft.Fluids.ModFluids;
 import com.lordpine.chemicraft.GUI.GuiHandler;
 import com.lordpine.chemicraft.Items.ModItems;
 import com.lordpine.chemicraft.Recipes.RecipeRegistry;
-import com.lordpine.chemicraft.TileEntities.ModTiles;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.network.NetworkRegistry;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = ChemiCraft.MODID, name = "ChemiCraft", version = ChemiCraft.VERSION)
 public class ChemiCraft
 {
+    public static final String packetChannel = "ChemicraftData";
     public static final String MODID = "chemicraft";
     public static final String VERSION = "0.01";
 
@@ -32,11 +28,15 @@ public class ChemiCraft
     public static ChemiCraft instance;
 
     @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ModBlocks.registerBlocks();
+    }
+
+    @EventHandler
     public void init(FMLInitializationEvent event)
     {
         ModFluids.registerFluids();
         ModItems.registerItems();
-        ModBlocks.registerBlocks();
         proxy.registerTiles();
         proxy.registerRenderers();
         MinecraftForge.EVENT_BUS.register(ModEventHandler.INSTANCE);
